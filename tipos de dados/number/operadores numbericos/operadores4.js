44d899cbb457ae07c1bf8f0623f0fb852c8a7959
Rafael Geronimo <e-rafaelg@weg.net>
2023-09-12 07:27:42 -0300

    exemplos operadores numericos

--- tipos de dados/number/operadores numbericos/operadores4.js
+++ tipos de dados/number/operadores numbericos/operadores4.js
@@ -8,3 +8,16 @@ console.log(128>>1); // shift direita: desloca o bit pra direita - como se divid
 console.log(-2>>>1); //shift direita trocando sinal: rotação que leva o sinal junto.
 
 console.log((5).toString(2).padStart(32,0)); // escrevendo 5 bit a bit
+
+// -2 >>> 1
+// 2147483647
+// > (-2 >>> 0).toString(2).padStart(32,0)
+// '11111111111111111111111111111110'
+// >
+// (To exit, press Ctrl+C again or Ctrl+D or type .exit)
+// >
+// PS C:\Users\e-rafaelg\JSMasterclass> node
+// Welcome to Node.js v16.16.0.      
+// Type ".help" for more information.
+// > (2147483647).toString(2).padStart(32,0)
+// '01111111111111111111111111111111'
