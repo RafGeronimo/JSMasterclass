6b27eebf46bcc33f494315826c24566c3275c5ca
e-rafaelg <e-rafaelg@weg.net>
2023-09-26 07:49:07 -0300

    exercicio 1 - um dia vai

--- RegExp/regexp13.js
+++ RegExp/regexp13.js
@@ -5,3 +5,4 @@ console.log(result1.index);
 let result2 = regExp.exec("mary@hotmail.com;john@gmail.com");
 console.log(result2[0]);
 console.log(result2.index);
+console.log(result1[0]);
--- exercicios/E1.js
+++ exercicios/E1.js
@@ -1,8 +1,8 @@
 const command =
   "create table author (id number, name string, age number, city string, state string, country string)";
 
-const regExp = /create\stable\s([a-zA-Z]{1,})+\s(\([a-zA-Z]{1,})+/;
+const regExp = /create\stable\s([a-zA-Z]+)\s\(([a-zA-Z]+\s?[a-zA-Z]+){1,},\s?\)/;
 
-const result = regExp.exec("create table rafael (id number)");
+const result = regExp.exec("create table rafael (id number, )");
 const author = result;
 console.log("tableAuthor = ", author);
