a2d0df469b907667925c6107af236f835fca1341
RafGeronimo <rafaelgeronimo96@gmail.com>
2023-09-26 22:07:57 -0300

    E1

--- exercicios/E1.js
+++ exercicios/E1.js
@@ -1,8 +1,11 @@
 const command =
   "create table author (id number, name string, age number, city string, state string, country string)";
 
-const regExp = /create\stable\s([a-zA-Z]+)\s\(([a-zA-Z]+\s?[a-zA-Z]+){1,},\s?\)/;
+const regExp = /create\stable\s([a-zA-Z]+)\s\((([a-zA-Z\s$,]{1,}))\)/;
 
-const result = regExp.exec("create table rafael (id number, )");
-const author = result;
-console.log("tableAuthor = ", author);
+const result = regExp.exec(command);
+const tableName = result[1];
+console.log("tableName = ", tableName);
+
+const columns = result[2].split(",");
+console.log("columns = ", columns);
