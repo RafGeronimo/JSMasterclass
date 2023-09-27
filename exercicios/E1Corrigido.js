2a348f96e223f497446af75653feba4a643ee435
e-rafaelg <e-rafaelg@weg.net>
2023-09-27 07:33:11 -0300

    E1 final

--- exercicios/E1Corrigido.js
+++ exercicios/E1Corrigido.js
@@ -9,7 +9,7 @@ console.log("tableName = ", tableName);
 const columns = result[2].split(", ");
 console.log("columns = ", columns);
 
-
+//voce tem que chegar o no mesmo resultado, mas não com a mesma implementação.
 //Dúvidas: 
 // 1. Porque minha regExp original (antes da correcao) duplicava o 'id number, name string...' ?
 // 2. Qual a diferença entre utilizar o "match" e o "exec" nesse caso? ambos funcionaram (tanto na correção do Branas quanto no meu exercicio corrigido)
