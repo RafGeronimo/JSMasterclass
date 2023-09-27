ad9cdba18b982c06d91acad85dd56e918eeee622
e-rafaelg <e-rafaelg@weg.net>
2023-09-27 07:01:01 -0300

    E1V0 complete

--- exercicios/E1.js
+++ exercicios/E1.js
@@ -1,7 +1,7 @@
 const command =
   "create table author (id number, name string, age number, city string, state string, country string)";
 
-const regExp = /create\stable\s([a-zA-Z]+)\s\((([a-zA-Z\s$,]{1,}))\)/;
+const regExp = /create\stable\s([a-zA-Z]+)\s\((([a-zA-Z\s$,]+))\)/;
 
 const result = regExp.exec(command);
 const tableName = result[1];
@@ -9,3 +9,17 @@ console.log("tableName = ", tableName);
 
 const columns = result[2].split(",");
 console.log("columns = ", columns);
+
+const idColumn = columns.slice(0,1).toString();
+const nameColumn = columns.slice(1,2).toString().trimStart();
+const ageColumn = columns.slice(2,3).toString().trimStart();
+const cityColumn = columns.slice(3,4).toString().trimStart();
+const stateColumn = columns.slice(4,5).toString().trimStart();
+const countryColumn = columns.slice(5,6).toString().trimStart();
+
+console.log(idColumn);
+console.log(nameColumn);
+console.log(ageColumn);
+console.log(cityColumn);
+console.log(stateColumn);
+console.log(countryColumn);
