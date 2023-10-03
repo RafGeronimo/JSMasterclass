90356c74468bb21a989120e9aa686c332eca9427
e-rafaelg <e-rafaelg@weg.net>
2023-10-03 09:02:03 -0300

    json

---
+++ objetos e funções/JSON/json2.js
@@ -0,0 +1,8 @@
+// jogar no console node:
+JSON.parse('10');
+JSON.parse('"JavaScript"');
+JSON.parse('true');
+JSON.parse('false');
+JSON.parse('{"name": "Self","paradigm": "OO"}');
+JSON.parse('[1,2,3,4,5,6,7,8,9]');
+JSON.parse('null');
