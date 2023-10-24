61172e0e321b4e6981f023c02e3223373d18938f
e-rafaelg <e-rafaelg@weg.net>
2023-10-24 07:44:05 -0300

    Exercicio 4 feito e corrigido

---
+++ exercicios/E4.js
@@ -0,0 +1,45 @@
+const DatabaseError = function (statement, message) {
+    this.statement = statement;
+    this.message = message;
+    this.errorMessage = () => {
+        `${this.message}:` + `${this.statement}`;
+    }
+};
+
+const database = {
+    tables: {},
+    createTable(statement) {
+        const regExp = /create table ([a-zA-Z]+) \((.+)\)/;
+        const parsedStatement = statement.match(regExp);
+        const tableName = parsedStatement[1];
+        this.tables[tableName] = {
+            columns: {},
+            data: [],
+        };
+        let columns = parsedStatement[2];
+        columns = columns.split(", ");
+        for (let column of columns) {
+            column = column.split(" ");
+            const name = column[0];
+            const type = column[1];
+            this.tables[tableName].columns[name] = type;
+        }
+    },
+    execute(statement) {
+        if (statement.startsWith("create table")) {
+            return this.createTable(statement);
+        }else{   
+            return new DatabaseError(statement, "Syntax error");
+        }
+    },
+
+};
+
+try{
+    //database.execute("create table author (id number, name string, age number, city string, state string, country string)");
+    database.execute("select id, name from author")
+    console.log(JSON.stringify(database, null,"   "));
+}catch(e){
+    console.log(e.errorMessage);
+};
+
