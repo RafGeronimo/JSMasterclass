0fd096884d998c99b47ab477f822e54ebcfe6ff7
e-rafaelg <e-rafaelg@weg.net>
2023-10-25 08:05:28 -0300

    destructuring

---
+++ objetos e funções/destructuring/destructuring4.js
@@ -0,0 +1,5 @@
+// const [name, , year] = "C;Dennis Ritchie;1972".split(";");
+// console.log(name, year);
+
+const [, author, year] = "C;Dennis Ritchie;1972".split(";");
+console.log(author, year);
---
+++ objetos e funções/destructuring/destructuring9.js
@@ -0,0 +1,11 @@
+const language = {
+    name: "C",
+    author: "Dennis Ritchie",
+    year: 1972
+};
+
+const {name:n, author:a, year:y} = language;
+
+console.log(n);
+console.log(a);
+console.log(y);
