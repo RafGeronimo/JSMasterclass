586303800f2fac578ea871ed2fac4bbe0a07fb43
e-rafaelg <e-rafaelg@weg.net>
2023-11-07 07:36:18 -0300

    E5Correcao e E5Corrigido

--- exercicios/E5Corrigido.js
+++ exercicios/E5Corrigido.js
@@ -8,45 +8,42 @@ const database = {
     createTable(statement) {
         const regExp = /create table ([a-zA-Z]+) \((.+)\)/;
         const parsedStatement = statement.match(regExp);
-        const tableName = parsedStatement[1];
+        let [, tableName, columns] = parsedStatement;
         this.tables[tableName] = {
             columns: {},
             data: [],
         };
-        let columns = parsedStatement[2];
         columns = columns.split(", ");
         for (let column of columns) {
             column = column.split(" ");
-            const name = column[0];
-            const type = column[1];
+            const [name, type] = column;
             this.tables[tableName].columns[name] = type;
         }
     },
     execute(statement) {
         if (statement.startsWith("create table")) {
             return this.createTable(statement);
-        }  
-        
-        else if(statement.startsWith("insert into")) {
-            return this.insert(statement);
         }
-        
+        else if (statement.startsWith("insert into")) {
+            return this.insert(statement);
+        } 
         const message = `Syntax error: "${statement}"`
         throw new DatabaseError(statement, message);
+        
     },
-    insert(statement) {
-        const regExp = /insert into ([a-zA-Z]+) \((.+)\) values \((.+)\)/;
-        const parsedStatement = statement.match(regExp);
-        const tableName = parsedStatement[1];
-        let columns = parsedStatement[2];
-        columns = columns.split(", ");
-        let values = parsedStatement[3];
-        values = values.split(", ");
-
+    insert(statement){
+        const regExp = /insert into ([a-zA-Z]+) \((.+)\) values \((.+)\)/
+        const parsedStatement = statement.match(regExp)
+        let [, tableName, columns, values] = parsedStatement;
+        columns = columns.split(", ")
+        values = values.split(", ")
+    
         let row = {}
         for (let i = 0; i < columns.length; i++){
-            Object.defineProperty(row, [columns[i]], {enumerable: true, value: values[i]})
-        }
+            const column = columns[i];
+            const value = values[i]
+            row[column] = value
+        };
         this.tables[tableName].data.push(row);
     }
 
@@ -57,7 +54,6 @@ try{
     database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)")
     database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)");
     database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)");
-    //database.execute("select id, name from author")
     console.log(JSON.stringify(database, null,"   "));
 }catch(e){
     console.log(e.message);
