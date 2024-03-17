a48dd37f586dc8c4f3d07b25e68aa982ba3f9a0f
RafGeronimo <rafaelgeronimo96@gmail.com>
2024-03-17 18:55:33 -0300

    E11 completed

---
+++ exercicios/E11/database.mjs
@@ -0,0 +1,74 @@
+import DatabaseError from "./databaseError.mjs";
+import Parser from "./parser.mjs";
+
+export default class Database {
+  constructor() {
+    this.tables = {};
+    this.parser = new Parser();
+  }
+  createTable(parsedStatement) {
+    let [, tableName, columns] = parsedStatement;
+    this.tables[tableName] = {
+      columns: {},
+      data: [],
+    };
+    columns = columns.split(", ");
+    for (let column of columns) {
+      column = column.split(" ");
+      const [name, type] = column;
+      this.tables[tableName].columns[name] = type;
+    }
+  }
+  insert(parsedStatement) {
+    let [, tableName, columns, values] = parsedStatement;
+    columns = columns.split(", ");
+    values = values.split(", ");
+
+    let row = {};
+    for (let i = 0; i < columns.length; i++) {
+      const column = columns[i];
+      const value = values[i];
+      row[column] = value;
+    }
+    this.tables[tableName].data.push(row);
+  }
+  select(parsedStatement) {
+    let [, columns, tableName, whereClause] = parsedStatement;
+    columns = columns.split(", ");
+    let rows = this.tables[tableName].data;
+    if (whereClause) {
+      const [columnWhere, valueWhere] = whereClause.split(" = ");
+      rows = rows.filter(function (row) {
+        return row[columnWhere] === valueWhere;
+      });
+    }
+    rows = rows.map(function (row) {
+      let selectedRow = {};
+      columns.forEach(function (column) {
+        selectedRow[column] = row[column];
+      });
+      return selectedRow;
+    });
+    return rows;
+  }
+  delete(parsedStatement) {
+    let [, tableName, whereClause] = parsedStatement;
+    let rows = this.tables[tableName].data;
+    whereClause = whereClause.split(" = ");
+    const [columnWhere, valueWhere] = whereClause;
+    rows = rows.filter((row) => valueWhere !== row[columnWhere]);
+    this.tables[tableName].data = rows;
+  }
+  execute(statement) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const result = this.parser.parse(statement);
+        if (result) {
+          resolve(this[result.command](result.parsedStatement));
+        }
+        const message = `Syntax error: "${statement}"`;
+        reject(new DatabaseError(statement, message));
+      }, 1000);
+    });
+  }
+}
